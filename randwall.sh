1d6ee206210bcefce12355fd2dc2abd40a071e74
coronelcortez <cuervo1155@gmail.com>
2021-05-25 14:02:03 -0300

    Primera version

---
+++ randwall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Seteo variables de tamaño
+anchodeseado="1920"
+altodeseado="1080"
+#Setea el path con las imagenes en una variable
+fuente=/home/$USER/Pictures/Wallpapers
+cd $fuente
+#Declara y carga archivos del directorio al array
+declare -a array
+for file in *.jpg
+do
+    imagenes=("${imagenes[@]}" "$file")
+done
+#Elige una imagen del array de forma aleatoria
+wallpaper="${imagenes[$RANDOM % ${#imagenes[@]}]}"
+#echo $wallpaper
+    #Obtiene medidas de la imagen previamente seleccionada
+    ancho=$(identify -format '%w' $wallpaper)
+    alto=$(identify -format '%h' $wallpaper)
+#echo "La imagen seleccionada tiene $ancho de ancho y $alto de alto"
+
+    if [ $ancho -eq $anchodeseado -a $alto -eq $altodeseado ] ;
+        then
+        echo "Tenemos fondo de pantalla FullHD $fuente/$wallpaper"
+#Setea la imagen seleccionada como wallpaper de forma repetida       
+        gsettings set org.cinnamon.desktop.background picture-uri "file://$fuente/$wallpaper" ;
+    else 
+        echo "No tenemos fondo de pantalla :("
+    fi
